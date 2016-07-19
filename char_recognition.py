4fb76a6d162a5d9aacb33d32f4bfca504241cbc6
Luo <luozl1993@gmail.com>
2016-07-19 18:18:01 +0000

    added test and accuracy

--- char_recognition.py
+++ char_recognition.py
@@ -26,6 +26,9 @@ class Config():
       self.debug = json_data['debug']
       self.debug_size = json_data['debug_size']
       self.test_only = json_data['test_only']
+      self.test_every_n_steps = json_data['test_every_n_steps']
+      self.test_size = json_data['test_size']
+      self.test_size = self.test_size-self.test_size%self.batch_size
 
 class CNN_Model():
   def __init__(self, config):
@@ -39,8 +42,8 @@ class CNN_Model():
   def load_data(self, debug=False, test_only=False):
     filename_test = os.path.join(self.config.dataset_dir, 'test.hdf5')
     f_test = h5py.File(filename_test, 'r')
-    self.char_imgs_test = f_test.get('char_imgs')
-    self.chars_embed_test = f_test.get('chars_embed')
+    self.char_imgs_test = np.array(f_test.get('char_imgs'), dtype=np.uint8)
+    self.chars_embed_test = np.array(f_test.get('chars_embed'), dtype=np.uint8)
     logger.info('loading test data (%d examples)', self.char_imgs_test.shape[0])
     f_test.close()
 
@@ -76,6 +79,9 @@ class CNN_Model():
   def add_loss_op(self, logits):
     losses = tf.nn.softmax_cross_entropy_with_logits(logits, self.labels_placeholder)
     loss = tf.reduce_mean(losses)
+
+    self.diff = tf.argmax(logits, 1)-tf.argmax(self.labels_placeholder, 1)
+
     return loss
 
   def add_training_op(self, loss):
@@ -92,18 +98,50 @@ def main():
   with tf.Session() as session:
     session.run(init)
 
-    iterator_train = utils.data_iterator_char(model.char_imgs_train, model.chars_embed_train, model.config.num_epochs, model.config.batch_size, model.config.embed_size)
+    iterator_train = utils.data_iterator_char(model.char_imgs_train,
+        model.chars_embed_train, model.config.num_epochs,
+        model.config.batch_size, model.config.embed_size)
 
     num_chars = model.char_imgs_train.shape[0]
 
     losses_train = []
+    accuracies_train = []
     cur_epoch = 0
     step_epoch = 0
     for step, (inputs_train, labels_train, epoch_train) in enumerate(iterator_train):
+      # test
+      if step%model.config.test_every_n_steps == 0:
+        losses_test = []
+        accuracies_test = []
+        iterator_test = utils.data_iterator_char(model.char_imgs_test,
+            model.chars_embed_test, 1,
+            model.config.batch_size, model.config.embed_size)
+
+        for step_test, (inputs_test, labels_test, epoch_test) in enumerate(iterator_test):
+          feed_test = {model.inputs_placeholder: inputs_test,
+                       model.labels_placeholder: labels_test,
+                       model.keep_prob_placeholder: 1.0}
+
+          ret_test = session.run([model.loss, model.diff],feed_dict=feed_test)
+          losses_test.append(ret_test[0])
+          accuracies_test.append(float(np.sum(ret_test[1] == 0))/ret_test[1].shape[0])
+
+        logger.info('<-------------------->')
+        logger.info('average test loss: %f (#batches = %d)',
+            np.mean(losses_test), len(losses_test))
+        logger.info('average test accuracy: %f (#batches = %d)',
+            np.mean(accuracies_test), len(accuracies_test))
+        logger.info('<-------------------->')
+
+        if model.config.test_only:
+          return
+
       # new epoch, calculate average loss from last epoch
       if epoch_train != cur_epoch:
-        logger.info('average training loss in epoch %d: %f\n', cur_epoch,
+        logger.info('average training loss in epoch %d: %f', cur_epoch,
             np.mean(losses_train[step_epoch:]))
+        logger.info('average training accuracy in epoch %d: %f', cur_epoch,
+            np.mean(accuracies_train[step_epoch:]))
         step_epoch = step
         cur_epoch = epoch_train
 
@@ -111,8 +149,9 @@ def main():
                     model.labels_placeholder: labels_train,
                     model.keep_prob_placeholder: model.config.keep_prob}
 
-      ret_train = session.run([model.train_op, model.loss], feed_dict=feed_train)
+      ret_train = session.run([model.train_op, model.loss, model.diff], feed_dict=feed_train)
       losses_train.append(ret_train[1])
+      accuracies_train.append(float(np.sum(ret_train[2] == 0))/ret_train[2].shape[0])
     #   logger.info('epoch %d, step %d: training loss = %f', epoch_train, step,
     #       ret_train[1])
 
