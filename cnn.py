092035520a2f3bc2c6ab1696a4125409c47acdd6
Luo <luozl1993@gmail.com>
2016-07-26 00:23:45 +0000

    added batch normalization

--- cnn.py
+++ cnn.py
@@ -6,15 +6,22 @@ from spatial_transformer import transformer
 def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
   # x: batch_size x height x width x depth
 
+  eps = 1e-5
+
   # localisation network
   with tf.variable_scope('loc-conv1') as scope:
     W_loc_conv1 = tf.get_variable('Weight', [5, 5, depth, 20],
         initializer=tf.contrib.layers.xavier_initializer())
     b_loc_conv1 = tf.get_variable('Bias', [20],
         initializer=tf.constant_initializer(0))
-    h_loc_conv1 = tf.nn.relu(tf.nn.conv2d(x, W_loc_conv1, strides=[1, 1, 1, 1],
-        padding='SAME') + b_loc_conv1)
-    h_loc_pool1 = tf.nn.max_pool(h_loc_conv1, ksize=[1, 2, 2, 1],
+    z_loc_conv1 = tf.nn.conv2d(x, W_loc_conv1, strides=[1, 1, 1, 1],
+        padding='SAME') + b_loc_conv1
+    mean_loc_conv1, variance_loc_conv1 = tf.nn.moments(z_loc_conv1, [0, 1, 2], keep_dims=False)
+    gamma_loc_conv1 = tf.get_variable('Gamma', [20], initializer=tf.constant_initializer(1))
+    beta_loc_conv1 = tf.get_variable('Beta', [20], initializer=tf.constant_initializer(0))
+    bn_loc_conv1 = tf.nn.batch_normalization(z_loc_conv1, mean_loc_conv1, variance_loc_conv1, beta_loc_conv1, gamma_loc_conv1, eps)
+    a_loc_conv1 = tf.nn.relu(bn_loc_conv1)
+    h_loc_pool1 = tf.nn.max_pool(a_loc_conv1, ksize=[1, 2, 2, 1],
         strides=[1, 2, 2, 1], padding='SAME')
 
   with tf.variable_scope('loc-conv2') as scope:
@@ -22,9 +29,14 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
         initializer=tf.contrib.layers.xavier_initializer())
     b_loc_conv2 = tf.get_variable('Bias', [20],
         initializer=tf.constant_initializer(0))
-    h_loc_conv2 = tf.nn.relu(tf.nn.conv2d(h_loc_pool1, W_loc_conv2,
-        strides=[1, 1, 1, 1], padding='SAME') + b_loc_conv2)
-    h_loc_pool2 = tf.nn.max_pool(h_loc_conv2, ksize=[1, 2, 2, 1],
+    z_loc_conv2 = tf.nn.conv2d(h_loc_pool1, W_loc_conv2, strides=[1, 1, 1, 1],
+        padding='SAME') + b_loc_conv2
+    mean_loc_conv2, variance_loc_conv2 = tf.nn.moments(z_loc_conv2, [0, 1, 2], keep_dims=False)
+    gamma_loc_conv2 = tf.get_variable('Gamma', [20], initializer=tf.constant_initializer(1))
+    beta_loc_conv2 = tf.get_variable('Beta', [20], initializer=tf.constant_initializer(0))
+    bn_loc_conv2 = tf.nn.batch_normalization(z_loc_conv2, mean_loc_conv2, variance_loc_conv2, beta_loc_conv2, gamma_loc_conv2, eps)
+    a_loc_conv2 = tf.nn.relu(bn_loc_conv2)
+    h_loc_pool2 = tf.nn.max_pool(a_loc_conv2, ksize=[1, 2, 2, 1],
         strides=[1, 2, 2, 1], padding='SAME')
 
   with tf.variable_scope('loc-fc1') as scope:
@@ -42,7 +54,8 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
     initial = np.array([[1., 0, 0], [0, 1., 0]])
     initial = initial.astype('float32')
     initial = initial.flatten()
-    b_loc_fc2 = tf.get_variable('Bias', initializer=tf.constant(initial))
+    b_loc_fc2 = tf.get_variable('Bias', [6],
+        initializer=tf.constant_initializer(initial))
     h_loc_fc2 = tf.matmul(h_loc_fc1_drop, W_loc_fc2) + b_loc_fc2
 
   with tf.variable_scope('transformer') as scope:
@@ -54,9 +67,14 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
         initializer=tf.contrib.layers.xavier_initializer())
     b_conv1 = tf.get_variable('Bias', [32],
         initializer=tf.constant_initializer(0))
-    h_conv1 = tf.nn.relu(tf.nn.conv2d(x_trans, W_conv1, strides=[1, 1, 1, 1],
-        padding='SAME') + b_conv1)
-    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1],
+    z_conv1 = tf.nn.conv2d(x_trans, W_conv1, strides=[1, 1, 1, 1],
+        padding='SAME') + b_conv1
+    mean_conv1, variance_conv1 = tf.nn.moments(z_conv1, [0, 1, 2], keep_dims=False)
+    gamma_conv1 = tf.get_variable('Gamma', [32], initializer=tf.constant_initializer(1))
+    beta_conv1 = tf.get_variable('Beta', [32], initializer=tf.constant_initializer(0))
+    bn_conv1 = tf.nn.batch_normalization(z_conv1, mean_conv1, variance_conv1, beta_conv1, gamma_conv1, eps)
+    a_conv1 = tf.nn.relu(bn_conv1)
+    h_pool1 = tf.nn.max_pool(a_conv1, ksize=[1, 2, 2, 1],
         strides=[1, 2, 2, 1], padding='SAME')
 
   with tf.variable_scope('conv2') as scope:
@@ -64,9 +82,14 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
         initializer=tf.contrib.layers.xavier_initializer())
     b_conv2 = tf.get_variable('Bias', [32],
         initializer=tf.constant_initializer(0))
-    h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1],
-        padding='SAME') + b_conv2)
-    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1],
+    z_conv2 = tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1],
+        padding='SAME') + b_conv2
+    mean_conv2, variance_conv2 = tf.nn.moments(z_conv2, [0, 1, 2], keep_dims=False)
+    gamma_conv2 = tf.get_variable('Gamma', [32], initializer=tf.constant_initializer(1))
+    beta_conv2 = tf.get_variable('Beta', [32], initializer=tf.constant_initializer(0))
+    bn_conv2 = tf.nn.batch_normalization(z_conv2, mean_conv2, variance_conv2, beta_conv2, gamma_conv2, eps)
+    a_conv2 = tf.nn.relu(bn_conv2)
+    h_pool2 = tf.nn.max_pool(a_conv2, ksize=[1, 2, 2, 1],
         strides=[1, 2, 2, 1], padding='SAME')
 
   with tf.variable_scope('conv3') as scope:
@@ -74,9 +97,14 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
         initializer=tf.contrib.layers.xavier_initializer())
     b_conv3 = tf.get_variable('Bias', [64],
         initializer=tf.constant_initializer(0))
-    h_conv3 = tf.nn.relu(tf.nn.conv2d(h_pool2, W_conv3, strides=[1, 1, 1, 1],
-        padding='SAME') + b_conv3)
-    h_pool3 = tf.nn.max_pool(h_conv3, ksize=[1, 2, 2, 1],
+    z_conv3 = tf.nn.conv2d(h_pool2, W_conv3, strides=[1, 1, 1, 1],
+        padding='SAME') + b_conv3
+    mean_conv3, variance_conv3 = tf.nn.moments(z_conv3, [0, 1, 2], keep_dims=False)
+    gamma_conv3 = tf.get_variable('Gamma', [64], initializer=tf.constant_initializer(1))
+    beta_conv3 = tf.get_variable('Beta', [64], initializer=tf.constant_initializer(0))
+    bn_conv3 = tf.nn.batch_normalization(z_conv3, mean_conv3, variance_conv3, beta_conv3, gamma_conv3, eps)
+    a_conv3 = tf.nn.relu(bn_conv3)
+    h_pool3 = tf.nn.max_pool(a_conv3, ksize=[1, 2, 2, 1],
         strides=[1, 2, 2, 1], padding='SAME')
 
   with tf.variable_scope('fc1') as scope:
@@ -95,7 +123,11 @@ def CNN(x, height, width, depth, output_size, keep_prob, keep_prob_transformer):
         initializer=tf.constant_initializer(0))
     logits = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-  variables_spatial_transformer = [W_loc_conv1, b_loc_conv1, W_loc_conv2, b_loc_conv2, W_loc_fc1, b_loc_fc1, W_loc_fc2, b_loc_fc2]
-  variables_CNN = [W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W_fc1, b_fc1, W_fc2, b_fc2]
+  variables_spatial_transformer = [W_loc_conv1, b_loc_conv1, gamma_loc_conv1,
+      beta_loc_conv1, W_loc_conv2, b_loc_conv2, gamma_loc_conv2, beta_loc_conv2,
+      W_loc_fc1, b_loc_fc1, W_loc_fc2, b_loc_fc2]
+  variables_CNN = [W_conv1, b_conv1, gamma_conv1, beta_conv1, W_conv2, b_conv2,
+      gamma_conv3, beta_conv3, W_conv3, b_conv3, gamma_conv3, beta_conv3, W_fc1,
+      b_fc1, W_fc2, b_fc2]
 
   return logits, variables_spatial_transformer, variables_CNN, x_trans
